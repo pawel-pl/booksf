c2c9e80fcac4bc0796c1c00bdd713d2d7b845a2d
pawelooo84@gmail.com <pawelooo84@gmail.com@f299c115-f082-0c72-e3f0-cc91350dcd1c>
2011-03-21 23:17:32 +0000

    mdb

--- EjbTest/core/src/test/java/client/ch10/Client_1.java
+++ EjbTest/core/src/test/java/client/ch10/Client_1.java
@@ -1,8 +1,13 @@
 package client.ch10;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+
+import javax.jms.Connection;
+import javax.jms.Queue;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 
 import client.ctx.provider.ServiceLocator;
 import core.ejb.logic.travelagent.TravelAgentRemote;
@@ -17,7 +22,7 @@ public class Client_1 {
 	    TravelAgentRemote travelAgent = ServiceLocator.getInstance()
 		    .getTravelAgent();
 
-	    //initDB();
+	    // initDB();
 
 	    // travelAgent.removeAndMerge();
 	    // travelAgent.merge();
@@ -26,19 +31,45 @@ public class Client_1 {
 	    // String query =
 	    // "select c.lastName, count(res), sum(res.totalPaid) from Customer c left join c.reservations res group by c.lastName having sum(res.totalPaid) > 2 order by c.lastName desc";
 
-	    String query = "select c.CUST_ID, c.FIRST_NAME, c.LAST_NAME, c.city, c.houseNo, c.street, c.card_id, cc_id from CUSTOMER c, CreditCard cc where cc.cc_id = c.card_id"; //left join reservation r on r.cust_id = c.cust_id group by c.cust_id";
-	    List<?> result = travelAgent.executeNativeQuery(query,
-		    "customerMapping");// travelAgent.executeQuery(query);
-
-	    System.out.println(result);
+	    /*
+	     * String query =
+	     * "select c.CUST_ID, c.FIRST_NAME, c.LAST_NAME, c.city, c.houseNo, c.street, c.card_id, cc_id from CUSTOMER c, CreditCard cc where cc.cc_id = c.card_id"
+	     * ; //left join reservation r on r.cust_id = c.cust_id group by
+	     * c.cust_id"; List<?> result =
+	     * travelAgent.executeNativeQuery(query, "customerMapping");//
+	     * travelAgent.executeQuery(query);
+	     * 
+	     * System.out.println(result);
+	     */
+	    travelAgent.testMessaging();
+	    sendMessage();
 
 	} catch (Exception ne) {
 	    ne.printStackTrace();
 	}
     }
 
+    private static void sendMessage() throws Exception {
+
+	QueueConnectionFactory jmsConnFactory = ServiceLocator.getInstance()
+		.getConnectionFactory();
+	Queue sl2MdbQueue = ServiceLocator.getInstance().getSl2MdbQueue();
+
+	Connection conn = jmsConnFactory.createConnection();
+	javax.jms.Session session = conn.createSession(false,
+		Session.AUTO_ACKNOWLEDGE);
+	javax.jms.MessageProducer producer = session
+		.createProducer(sl2MdbQueue);
+	TextMessage msg = session.createTextMessage("Client to MDB");
+	msg.setStringProperty("type", "type2");
+	producer.send(msg);
+
+	conn.close();
+    }
+
+    @SuppressWarnings("unused")
     private static void initDB() throws Exception {
-	
+
 	TravelAgentRemote travelAgent = ServiceLocator.getInstance()
 		.getTravelAgent();
 
